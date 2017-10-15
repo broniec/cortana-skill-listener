8d129963c6dc22b2f9f9b1058eaded49859576e1
William Broniec <williambroniec@gmail.com>
2017-10-15 09:42:06 -0400

    Updated Cortana Code (Audio)

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -1,9 +1,12 @@
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.Dialogs;
+using System.Net;
 using System.Net.Http;
+
 
 
 namespace Microsoft.Bot.Sample.SimpleEchoBot
@@ -18,8 +21,34 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
 
         public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
         {
+            Activity activity = (Activity) context.Activity;
+            Activity reply = activity.CreateReply();
+
             var message = await argument;
-            await context.PostAsync("You said: " + message.Text);
+            
+            string html = string.Empty;
+
+            string url = @"http://fabriceflask.azurewebsites.net/process?query=" + message.Text;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            // set output
+            reply.Speak = html;
+            reply.Text = html;
+            
+            // auto turn on mic after output
+            // maybe not essential (?)
+            reply.InputHint = InputHints.ExpectingInput;
+
+            await context.PostAsync(reply);
             context.Wait(MessageReceivedAsync);
         }
     }
